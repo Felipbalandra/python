d8ad3c74d98e0f0c6c8d5f79bb3497ab8faa2b24
root <root@localhost>
2023-05-02 17:58:24 +0800

    auto publish 1683021503461

---
+++ src/main.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Welcome</h1>
+<p>
+    Welcome to the <i>Hello World</i> Project. <br>
+    This is the index page rendered from the templates <br>
+    directory. Congratulations for making this far.
+</p>
+
+</body>
+</html>
